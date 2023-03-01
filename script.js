78a7c3c3db5517569677e4987438fb86cf82a4be
Jishan040 <118433367+Jishan040@users.noreply.github.com>
2023-03-01 13:18:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,12 @@
-//your code here!
-const list = document.getElementById('list');
-
-// Add 10 list items by default
-for (let i = 1; i <= 10; i++) {
-  const listItem = document.createElement('li');
-  listItem.textContent = `List item ${i}`;
-  list.appendChild(listItem);
-}
-
-// Add 2 more list items when the user reaches the end of the list
-list.addEventListener('scroll', () => {
-  const { scrollTop, scrollHeight, clientHeight } = list;
-  if (scrollTop + clientHeight >= scrollHeight) {
-    for (let i = 1; i < 2; i++) {
-      const listItem = document.createElement('li');
-      listItem.textContent = `List item ${list.children.length + i}`;
-      list.appendChild(listItem);
-    }
+const infinity = document.querySelector("#infi-list");
+let counter = 11;
+infinity.addEventListener("scroll", function () {
+  if (infinity.scrollHeight <= infinity.offsetHeight + infinity.scrollTop) {
+    const htmlContent = ` 
+    <li class="${counter}">Item ${counter++}</li>
+    <li class="${counter}">Item ${counter++}</li>
+    `;
+    infinity.insertAdjacentHTML("beforeend", htmlContent);
   }
 });
 
